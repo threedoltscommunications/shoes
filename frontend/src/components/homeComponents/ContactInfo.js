8a32ecb9db98bd1b490a17c2629486d7098854e6
threedoltswebsite <153194210+threedoltswebsite@users.noreply.github.com>
2024-05-27 18:47:05 +0300

    first commit

---
+++ frontend/src/components/homeComponents/ContactInfo.js
@@ -0,0 +1,40 @@
+import React from "react";
+import specifications from "../../specifications";
+
+const ContactInfo = () => {
+  return (
+    <div className="contactInfo container">
+      <div className="row">
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-phone-alt"></i>
+            </div>
+            <h5>Call Us 24x7</h5>
+            <p>{specifications.number}</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-map-marker-alt"></i>
+            </div>
+            <h5>Headquarter</h5>
+            <p>{specifications.location}</p>
+          </div>
+        </div>
+        <div className="col-12 col-md-4 contact-Box">
+          <div className="box-info">
+            <div className="info-image">
+              <i className="fas fa-fax"></i>
+            </div>
+            <h5>WhatsAap</h5>
+            <p>{specifications.number}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactInfo;
---
+++ frontend/src/specifications.js
@@ -0,0 +1,7 @@
+const specifications = {
+  number: "0712012113",
+  email: "threedoltswebsites@gmail.com",
+  logo: "Three Dolts",
+  location: "Nairobi, Kenya",
+};
+export default specifications;
